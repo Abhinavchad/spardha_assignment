56caa0031c5ee4625f795d3b8a2e87ed212d1d6f
Abhinav <abhinav.chaudhary.mec22@itbhu.ac.in>
2023-08-17 00:19:38 +0530

    Your commit message here

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,39 +16,40 @@ const LoginForm = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // You can add your login logic here, such as sending a request to a server.
     console.log('Username:', username);
     console.log('Password:', password);
   };
 
   return (
-    <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}} className="signup-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-        <button className="signup-button" type="submit">Login</button>
-      </form>
+    <div className='auth-box'>
+      <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}} className="signup-container">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={handleUsernameChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+          </div>
+          <button className="signup-button" type="submit">Login</button>
+        </form>
       
-    </div>
+      </div>
+      </div>
   );
 };
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -32,45 +32,51 @@ const Signup = () => {
   return (
     <div className='auth-box'>
       <h2>Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          name='firstName'
-          placeholder='First Name'
-          value={formData.firstName}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type='text'
-          name='lastName'
-          placeholder='Last Name'
-          value={formData.lastName}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type='email'
-          name='email'
-          placeholder='Email'
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type='password'
-          name='password'
-          placeholder='Password'
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
+      <form onSubmit={handleSubmit} className='form-container'>
+        <div className='input-group'>
+          <input
+            type='text'
+            name='firstName'
+            placeholder='First Name'
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='input-group'>
+          <input
+            type='text'
+            name='lastName'
+            placeholder='Last Name'
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='input-group'>
+          <input
+            type='email'
+            name='email'
+            placeholder='Email'
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='input-group'>
+          <input
+            type='password'
+            name='password'
+            placeholder='Password'
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
         <button type='submit'>Sign Up</button>
       </form>
-      
     </div>
   );
 };
 
 export default Signup;
-
